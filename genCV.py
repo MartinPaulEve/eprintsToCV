3d65adc179c9a5ecaa71438af1779deba73d4c59
Martin Paul Eve <martin@martineve.com>
2019-04-14 08:59:12 +0100

    Move citeproc server shutdown to finally block

--- genCV.py
+++ genCV.py
@@ -63,22 +63,26 @@ def main(args):
 
     citeproc = None
 
-    # start the citeproc server if the flag is passed
-    if '--citeproc' in args and args['--citeproc']:
-        citeproc = CiteProc(config, logger)
+    try:
+        # start the citeproc server if the flag is passed
+        if '--citeproc' in args and args['--citeproc']:
+            citeproc = CiteProc(config, logger)
 
-    if 'fetch' in args and args['fetch']:
-        if len(args['TYPES']) > 0:
-            repo.fetch(args['TYPES'])
-        else:
-            repo.fetch(config.default_types)
+        if 'fetch' in args and args['fetch']:
+            if len(args['TYPES']) > 0:
+                repo.fetch(args['TYPES'])
+            else:
+                repo.fetch(config.default_types)
 
-    elif 'make' in args and args['make']:
-        template_builder = TemplateBuilder(repo, config, logger)
-        template_builder.build(args['OUTPUT_TYPES'])
-
-    if '--citeproc' in args and args['--citeproc']:
-        citeproc.shutdown()
+        elif 'make' in args and args['make']:
+            template_builder = TemplateBuilder(repo, config, logger)
+            template_builder.build(args['OUTPUT_TYPES'])
+    except:
+        logger.error("An uncaught error was thrown.")
+    finally:
+        # always try to shutdown the citeproc server
+        if '--citeproc' in args and args['--citeproc']:
+            citeproc.shutdown()
 
 
 if __name__ == "__main__":
